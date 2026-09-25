8f880f89b2a01907dd692ba1f58b41ad47a591b0
Bushrareyaz <bushrareyaz699@gmail.com>
2026-09-25 19:46:50 +0530

    46th code added

---
+++ Day10code.py
@@ -0,0 +1,18 @@
+#code 46 ; Write a program to display the first n prime numbers.
+n = int(input("Enter how many prime numbers: "))
+count = 0
+num = 2
+
+while count < n:
+    is_prime = True
+
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num)
+        count += 1
+    num += 1
+
+        
